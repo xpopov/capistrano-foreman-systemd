1737684315b6b680e46989dbfa74d8058ae1140a
Max Popov <domain.stalker@gmail.com>
2022-09-19 02:06:53 +0600

    Update foreman_systemd.rb

--- lib/capistrano/foreman_systemd.rb
+++ lib/capistrano/foreman_systemd.rb
@@ -2,7 +2,7 @@ require "capistrano/plugin"
 module Capistrano
   class ForemanSystemd < Capistrano::Plugin
     def define_tasks
-      eval_rakefile File.expand_path('../tasks/nginx.rake', __FILE__)
+      eval_rakefile File.expand_path('../tasks/foreman_systemd.rake', __FILE__)
     end
   end
 end
